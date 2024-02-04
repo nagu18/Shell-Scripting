26c6bd0ad45c8f3c3d8cbc323bf73dcd5aeca042
nagendher <kondanagendher8@gmail.com>
2024-02-04 17:47:56 +0530

    Create Myscript.sh

---
+++ Myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#the $0 is use to konw the name of the script and it's parth.
+#the $* is used to take the input from user
+#example name of the script (Myscript.sh nagu) the out will be hello nagu 
+# view the code to konow it well.
+echo "the name of the script is: $0"
+echo "Hello $*" 
+exit 0
